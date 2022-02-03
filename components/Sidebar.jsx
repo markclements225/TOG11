1aa247560e96cccdc2038f5989430070ddc379ee
markclements225 <mcleme9@gmail.com>
2022-02-03 14:21:50 -0600

    sidebar icons added before restart needed

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,27 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/outline'
+
 export default function Sidebar() {
   return (
-    <div>
-      <h1>This is a dope sidebar</h1>
+    <div className="p-5 text-gray-500">
+      <div>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+      </div>
     </div>
   )
 }
